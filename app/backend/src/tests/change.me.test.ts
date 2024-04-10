4b3f34a6bed19103b6d7695a3712360539afd0a0
Lina Hsu <linahthsu@gmail.com>
2024-04-10 18:27:35 -0300

    feat - implements TDD-integration test for endpoint GET /teams

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -4,15 +4,50 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
+import SequelizeTeamModel from '../database/models/SequelizeTeamModel'
+import ITeam from '../Interfaces/ITeam'
+import teamMock from './mocks/team.mock';
 
 import { Response } from 'superagent';
+import { Model } from 'sequelize';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Seu teste', () => {
+  beforeEach(function () { sinon.restore(); });
+
+  let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(SequelizeTeamModel, "findAll")
+  //     .resolves(teamMock.teams as unknown as Model<ITeam>[]);
+  // });
+
+  // after(() => {
+  //   (SequelizeTeamModel.findAll as sinon.SinonStub).restore();
+  // })
+
+  it('Lista todos os times com sucesso', async function () {
+    // Arrange
+    // const findAllReturn = teamMock.teams.map((team) => SequelizeTeamModel.build(team));
+    // sinon.stub(SequelizeTeamModel, 'findAll').resolves(findAllReturn);
+    sinon
+      .stub(SequelizeTeamModel, "findAll")
+      .resolves(teamMock.teams as unknown as Model<ITeam>[]);
+
+    // Act
+    chaiHttpResponse = await chai.request(app).get('/teams');
+
+    // Assert
+    expect(chaiHttpResponse.status).to.be.eq(200);
+    expect(chaiHttpResponse.body).to.deep.equal(teamMock.teams);
+
+    (SequelizeTeamModel.findAll as sinon.SinonStub).restore();
+  });
+
   /**
    * Exemplo do uso de stubs com tipos
    */
@@ -38,8 +73,4 @@ describe('Seu teste', () => {
 
   //   expect(...)
   // });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
 });
---
+++ app/backend/src/tests/mocks/team.mock.ts
@@ -0,0 +1,23 @@
+const team = {
+  id: 1,
+  teamName: 'Palmeiras',
+};
+
+const teams = [
+  {
+    id: 1,
+    teamName: 'Palmeiras',
+  },
+  {
+    id: 2,
+    teamName: 'Corinthians',
+  },
+  {
+    id: 3,
+    teamName: 'São Paulo',
+  },
+];
+
+export default {
+  teams,
+};
