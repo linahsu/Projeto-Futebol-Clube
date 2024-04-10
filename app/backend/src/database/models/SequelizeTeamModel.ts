af3e46eb4de4c202671f6f33e5b009239767590e
Lina Hsu <linahthsu@gmail.com>
2024-04-10 17:33:25 -0300

    feat - implements migration and model for teams table

---
+++ app/backend/src/database/models/SequelizeTeamModel.ts
@@ -0,0 +1,47 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class SequelizeTeamModel extends Model<InferAttributes<SequelizeTeamModel>,
+InferCreationAttributes<SequelizeTeamModel>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+SequelizeTeamModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default SequelizeTeamModel;
