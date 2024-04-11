383569e8d7a45cd007eeef0652abd62bb0084a72
Lina Hsu <linahthsu@gmail.com>
2024-04-11 12:19:30 -0300

    feat - implements interface IUser, migrations and model for users table

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import teamRouter from './team.routes';
+import loginRouter from './login.routes';
 
 const router = Router();
 
+router.use('/login', loginRouter);
 router.use('/teams', teamRouter);
 
 export default router;
